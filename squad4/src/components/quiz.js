475b939cc5f57aeba97efa07df343db69b49399a
phtf92 <phtf92@gmail.com>
2020-03-28 21:13:53 -0300

    ansjdno

--- squad4/src/components/quiz.js
+++ squad4/src/components/quiz.js
@@ -31,23 +31,36 @@ class Quiz extends Component {
     if (i !== validAnswer - 1) {
       const newPush = [...wrongAnswers];
       newPush.push(questions[nQuestion][0])
-      this.setState({ wrongAnswers: newPush, nQuestion: nQuestion + 1 })
+      this.setState({ wrongAnswers: newPush, })
     }
+    this.setState({nQuestion: nQuestion + 1})
     if (n === 3) this.setState({ endGame: true })
   }
 
   render() {
     console.log(this.state);
-    const { questions, nQuestion, score, endGame } = this.state;
+    const { questions, nQuestion, score, endGame, wrongAnswers } = this.state;
     if (!questions.length) return <div>Loading...</div>
-    if (endGame) return <p>Fim de jogo</p>
+    if (endGame) return (
+      <div>
+        {wrongAnswers.map(ans => {
+          return (
+            <div>
+              <p>{ans.question}</p>
+              <p>{ans.options[ans.correct - 1]}</p>
+              <p>{ans.explanation}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
     console.log(questions[0]);
     return (
       <div>
         {questions[nQuestion][0].question}
         <ul>
           {questions[nQuestion][0].options.map((q, index) =>
-            <button onClick={() => this.validAnswer(index, questions[nQuestion][0].correct, nQuestion, score)}>{q}</button>
+            <button onClick={() => this.validAnswer(index, questions[nQuestion][0].correct, nQuestion)}>{q}</button>
           )}
         </ul>
       </div>
